c256771adeb963fea6954b712540f8a97e718337
Matic Leva <matic.leva@inova.si>
2019-07-08 12:53:59 +0200

    Create landing page

--- client/src/components/Landing/Landing.tsx
+++ client/src/components/Landing/Landing.tsx
@@ -1,43 +1,58 @@
 import * as React from 'react';
 import {FunctionComponent, useEffect} from 'react';
-import {Card, CardActionArea, CardMedia, CircularProgress} from '@material-ui/core';
-import {useObservable} from 'mobx-react-lite';
+import {Card, CardHeader, CardMedia, CircularProgress, Grid} from '@material-ui/core';
+import {observer, useObservable} from 'mobx-react-lite';
 import {Fetcher} from '../../store/fetcher';
 import {Main} from '../Layout/Main';
 import axios from 'axios';
 import {ErrorCallout} from '../ErrorCallout';
 
-export const Landing: FunctionComponent = () => {
+type LandingResponse = APIResponse<GalleryAlbumResponse[]>;
 
-    const fetcher = useObservable(new Fetcher<CustomGalleryResponse>());
+export const Landing: FunctionComponent = observer(() => {
+
+    const fetcher = useObservable(new Fetcher<LandingResponse>());
     useEffect(() => {
         fetcher
             .fetch(
-                axios.get('https://api.imgur.com/3/gallery/hot/time'),
+                axios.get<LandingResponse>(
+                    'https://api.imgur.com/3/gallery/hot/time',
+                    {
+                        headers: {
+                            Authorization: 'Client-ID 52680c0415d6925',
+                        },
+                    },
+                ),
                 Math.random().toString(10),
             )
             .catch(console.error);
     }, []);
 
-    const body = fetcher.error ? (
-        <ErrorCallout error={fetcher.error}/>
-    ) : fetcher.loading ? (
-        <CircularProgress/>
-    ) : fetcher.data ? (
-        fetcher.data.items.map(() => (
-            <Card>
-                <CardActionArea>
-                    <CardMedia
-                        image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg"
-                        title="Contemplative Reptile"
-                    />
-                </CardActionArea>
-            </Card>
-        ))
-    ) : null;
     return (
-        <Main heading={'Pigeon'}>
-            {body}
+        <Main heading={'Giraffe'}>
+            <Grid container spacing={1}>
+                {
+                    fetcher.error ? (
+                        <ErrorCallout error={fetcher.error}/>
+                    ) : fetcher.loading ? (
+                        <CircularProgress/>
+                    ) : fetcher.data ?
+                        fetcher.data.data
+                            .map(item => item.images && item.images.length > 0 ? (
+                                <Grid item xs={12}>
+                                    <Card key={item.id}>
+                                        <CardHeader title={item.title} subheader={item.account_url}/>
+                                        <CardMedia
+                                            component='img'
+                                            src={item.images[0].link}
+                                            title={item.title}
+                                        />
+                                    </Card>
+                                </Grid>
+                            ) : null)
+                            .filter(Boolean) : null
+                }
+            </Grid>
         </Main>
     );
-};
+});
--- client/src/components/Layout/Main.tsx
+++ client/src/components/Layout/Main.tsx
@@ -1,31 +1,20 @@
 import * as React from 'react';
 import {FunctionComponent} from 'react';
-import img from '../../assets/login-bg.jpg';
-import {AppBar as AppBarM, IconButton, Toolbar, Typography} from '@material-ui/core';
-import {Menu} from '@material-ui/icons';
+import img from '../../assets/zwartevilt.png';
+import {AppBar, Toolbar, Typography} from '@material-ui/core';
 import {createStyles, makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() =>
     createStyles({
-        root: {
-            height: '100%',
-            background: `center / cover no-repeat url(${img})`,
-            display: 'flex',
-            flexDirection: 'column',
+            root: {
+                minHeight: '100%',
+                background: `url(${img}) repeat`,
+            },
+            main: {
+                padding: 8,
+            },
         },
-        main: {
-            padding: 10,
-            flexGrow: 1,
-        },
-        navigation: {
-            position: 'relative' as 'relative',
-        },
-        fab: {
-            position: 'absolute' as 'absolute',
-            zIndex: 1,
-            bottom: 56 / 2,
-        },
-    }),
+    ),
 );
 
 type Props = {
@@ -36,14 +25,11 @@ export const Main: FunctionComponent<Props> = ({children, heading}) => {
     const classes = useStyles();
     return (
         <div className={classes.root}>
-            <AppBarM position="static">
+            <AppBar position={'static'}>
                 <Toolbar>
-                    <IconButton edge="start" color="inherit">
-                        <Menu/>
-                    </IconButton>
                     <Typography variant="h6">{heading}</Typography>
                 </Toolbar>
-            </AppBarM>
+            </AppBar>
             <main className={classes.main}>
                 {children}
             </main>
