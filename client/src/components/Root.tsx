ae5dea4b3371d72b4bb7fecaaf1f46e55812d325
Matic Leva <matic.leva@inova.si>
2019-07-08 14:03:34 +0200

    Add error reporting in error boundary

--- client/src/components/Root.tsx
+++ client/src/components/Root.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {ErrorInfo} from 'react';
 
 import 'normalize.css/normalize.css';
 
@@ -9,9 +10,14 @@ import {Provider as ServiceWorkerProvider} from './ServiceWorker/Context';
 import {UpdateNotification} from './ServiceWorker/UpdateNotification';
 import {Landing} from './Landing/Landing';
 import {ThemeProvider} from '@material-ui/styles';
-import {createMuiTheme, Theme} from '@material-ui/core';
+import {Button, Card, CardActions, CardContent, CardHeader, createMuiTheme, Theme, Typography} from '@material-ui/core';
 
-export class Root extends React.PureComponent {
+interface State {
+    error: Error | null,
+    info: ErrorInfo | null,
+}
+
+export class Root extends React.Component<{}, State> {
     private readonly theme: Theme;
 
     constructor(props: {}) {
@@ -21,14 +27,28 @@ export class Root extends React.PureComponent {
                 type: 'dark', // Switching the dark mode on is a single property value change.
             },
         });
+        this.state = {
+            error: null,
+            info: null,
+        };
     }
 
-    componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
         console.error(error);
         console.trace(errorInfo.componentStack);
+        this.setState({
+            error,
+            info: errorInfo,
+        });
     }
 
     public render() {
+        let githubIssue = '';
+        if (this.state.error && this.state.info) {
+            const title = encodeURIComponent(this.state.error.message);
+            const body = encodeURIComponent(this.state.error.message) + '\n\n' + encodeURIComponent(this.state.info.componentStack);
+            githubIssue = `https://github.com/DreadBoy/giraffe/issues/new?body=${body}&title=${title}`;
+        }
         return (
             <>
                 <Favicon/>
@@ -36,7 +56,22 @@ export class Root extends React.PureComponent {
                     <ThemeProvider theme={this.theme}>
                         <ServiceWorkerProvider>
                             <UpdateNotification/>
-                            <Landing/>
+                            {this.state.error ? (
+                                <Card>
+                                    <CardHeader title={'Oops, something went wrong'}/>
+                                    <CardContent>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            Please report this error by clicking button below.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button href={githubIssue} target={'_blank'}>
+                                            Report error
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            ) : (<Landing/>
+                            )}
                         </ServiceWorkerProvider>
                     </ThemeProvider>
                 </BrowserRouter>
