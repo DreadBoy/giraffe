30f435cd309ad7d528222b89c2347e0b1e5a411b
DreadBoy <DreadBoy@users.noreply.github.com>
2019-09-02 12:52:55 +0000

    Deploying to gh-pages - 12:52:55

---
+++ precache-manifest.f85385a8991f8f8a13d73f77b330242d.js
@@ -0,0 +1,21 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/giraffe/img/2583d8fce60a3542c19c33eb133993e7.png"
+  },
+  {
+    "url": "/giraffe/img/2d0efec9908cfb2029fa77caba533328.png"
+  },
+  {
+    "url": "/giraffe/img/7da4f80faa96b79b4df676769cc61ef3.png"
+  },
+  {
+    "url": "/giraffe/img/e20dd0613d06fef454e5e8500fb44536.png"
+  },
+  {
+    "revision": "e1b4af785f581a2f37d0bc34ae46ec90",
+    "url": "/giraffe/index.html"
+  },
+  {
+    "url": "/giraffe/js/bundle.10b8d8786394ddfdaa42.min.js"
+  }
+]);
